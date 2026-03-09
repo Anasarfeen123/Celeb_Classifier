e7685e6cda5418f06fb9b69f1d1a350b54929774
Anas Arfeen <Codecrusader07@gmail.com>
2026-03-09 20:04:24 +0530

    Better model

--- check.py
+++ check.py
@@ -1,2 +1,9 @@
 import tensorflow as tf
-print(tf.config.list_physical_devices("GPU"))
+
+print("GPUs:", tf.config.list_physical_devices("GPU"))
+
+a = tf.random.normal([1000, 1000])
+b = tf.random.normal([1000, 1000])
+c = tf.matmul(a, b)
+
+print(c)
